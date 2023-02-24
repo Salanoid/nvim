8d2d9620fc703f97b731e04c05d988346b940e8b
Salanoid <salanoid@gmail.com>
2023-02-24 22:10:14 +0200

    transparent background + some small fixes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.opt.guicursor = ""
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,3 +44,17 @@ require("lazy").setup({
     },
   },
 })
+
+require("notify").setup({
+  background_colour = "#282A36",
+})
+
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
