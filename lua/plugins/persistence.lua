21d7d6f5ff054956538ff10ae7eea88ee9b1a9f5
Salanoid <salanoid@gmail.com>
2023-04-16 22:26:59 +0300

    some fixes

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = function ()
+      require("persistence").setup({
+        options = {'buffers', 'curdir', 'globals', 'tabpages', 'winsize'},
+        pre_save = function() vim.api.nvim_exec_autocmds('User', { pattern = 'SessionSavePre' }) end,
+      })
+
+      require("scripts.auto-save-session").setup()
+    end
+  }
+}
+
