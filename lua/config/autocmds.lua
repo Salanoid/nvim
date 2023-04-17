8918fcae2d002824cc590e7e80d40574de5256da
Salanoid <salanoid@gmail.com>
2023-04-17 23:31:17 +0300

    more fixes for on cmds and copilot

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,28 @@ vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.spelllang = { "en" }
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.cursorline = true -- Enable highlighting of the current line
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.formatoptions = "jcroqlnt" -- tcqj
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+  vim.opt.splitkeep = "screen"
+  vim.opt.shortmess:append { C = true }
+end
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,12 @@
 return {
-  {
-    "github/copilot.vim",
-  },
+  { 'github/copilot.vim', config = function ()
+    vim.keymap.set("i", "<M-j>", "<Plug>(copilot-next)", { noremap = false })
+    vim.keymap.set("i", "<M-k>", "<Plug>(copilot-previous)", { noremap = false })
+    vim.keymap.set("i", "<M-l>", "<Plug>(copilot-suggest)", { noremap = false })
+
+    vim.cmd([[
+      imap <silent><script><expr> <M-p> copilot#Accept("\<CR>")
+    ]])
+  end }
 }
+
